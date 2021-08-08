5930c6a0b28ddde4111c865462ec518ebded2192
deps-repl <deps-repl@library>
2021-08-08 13:23:21 +0300

    i add process

--- src/deps_repl/core.clj
+++ src/deps_repl/core.clj
@@ -9,3 +9,26 @@
    [clojure.java.io :as io]
    [nrepl.server]
    [cider.nrepl]))
+
+(defn process
+  [opts]
+  (let [default-opts {:main-ns 'user
+                      :repl? true
+                      :nrepl? true
+                      :host "0.0.0.0"
+                      :port 7788}
+        {:keys [main-ns host port repl? nrepl?]} (merge default-opts opts)]
+
+    (eval (macroexpand `(require '[~main-ns])))
+
+    (when nrepl?
+      (nrepl.server/start-server :bind host
+                                 :port port
+                                 :handler (apply nrepl.server/default-handler cider.nrepl/cider-middleware)))
+
+    (when repl?
+      (apply clojure.main/main ["-e" (str (macroexpand `(do (in-ns '~main-ns) nil)))
+                                "-r"]))
+
+    (when-not nrepl?
+      (<!! (chan)))))
