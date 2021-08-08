2999cf0ae3c2c2273663d236c5f82346094b7e4b
deps-repl <deps-repl@library>
2021-08-08 13:22:45 +0300

    i add core

---
+++ src/deps_repl/core.clj
@@ -0,0 +1,11 @@
+(ns deps-repl.core
+  (:require
+   [clojure.core.async :as a :refer [chan go go-loop <! >! <!! >!!  take! put! offer! poll! alt! alts! close! onto-chan!
+                                     pub sub unsub mult tap untap mix admix unmix pipe
+                                     timeout to-chan  sliding-buffer dropping-buffer
+                                     pipeline pipeline-async]]
+   [clojure.string]
+   [clojure.spec.alpha :as s]
+   [clojure.java.io :as io]
+   [nrepl.server]
+   [cider.nrepl]))
