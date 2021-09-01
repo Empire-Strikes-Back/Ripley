0f618da94a913f4f661e33bdedf87c737e771cea
ripley <ripley@library>
2021-09-01 11:53:51 +0300

    i add process

--- src/ripley/core.clj
+++ src/ripley/core.clj
@@ -4,3 +4,16 @@
                                       pub sub unsub mix admix unmix dropping-buffer sliding-buffer pipeline pipeline-async to-chan! thread]]
     [clojure.string]
     [clojure.java.io :as io))
+
+
+(defn process
+  [opts]
+  (let [default-opts {:main-ns nil}
+        {:keys [main-ns]} (merge default-opts opts)]
+
+  (requiring-resolve 'main-ns)
+  
+  (apply clojure.main/main ["--eval" (format "(in-ns '%s)" main-ns)
+                            "--repl"])
+  )
+)
